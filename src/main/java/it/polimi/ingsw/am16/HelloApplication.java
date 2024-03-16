3e6fb39840ef9a5fb6b9b00fc283034035f2e6dd
Lorenzo Demontis <lorenzo.demontis@mail.polimi.it>
2024-03-16 13:37:11 +0100

    Game, GameModel, DrawType, GameState

--- src/main/java/it/polimi/ingsw/am16/HelloApplication.java
+++ src/main/java/it/polimi/ingsw/am16/HelloApplication.java
@@ -22,7 +22,7 @@ public class HelloApplication /* extends Application */ {
 
     public static void main(String[] args) {
         // launch();
-        System.out.println("Hello World");
+        System.out.println("レオナルド");
 
     }
 }
--- src/main/java/it/polimi/ingsw/am16/common/model/cards/Cornerable.java
+++ src/main/java/it/polimi/ingsw/am16/common/model/cards/Cornerable.java
@@ -4,4 +4,5 @@ package it.polimi.ingsw.am16.common.model.cards;
  * Interface implemented by all classes that represent an element that can be on one of a card's corners.
  */
 public interface Cornerable {
+
 }
