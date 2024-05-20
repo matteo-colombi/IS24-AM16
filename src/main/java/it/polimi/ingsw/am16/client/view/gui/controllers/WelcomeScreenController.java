94443d777fbefbdfe5dd2690fd0eefbb7f526e89
matteo-colombi <matteo.colombi@mail.polimi.it>
2024-05-21 00:39:00 +0200

    Implemented part of GUI main game functionality: setting common objectives, setting personal objective, setting the player's hand. Chat. Dragging and dropping cards in the play area. Adding cards to the play area. Setting the player's color and giving them the black peg (for the starting player)

--- src/main/java/it/polimi/ingsw/am16/client/view/gui/controllers/WelcomeScreenController.java
+++ src/main/java/it/polimi/ingsw/am16/client/view/gui/controllers/WelcomeScreenController.java
@@ -12,15 +12,33 @@ import java.net.URL;
 import java.rmi.RemoteException;
 import java.util.ResourceBundle;
 
+/**
+ * Controller for the welcome screen.
+ */
 public class WelcomeScreenController implements ScreenController, Initializable {
 
+    /**
+     * The text field for the username.
+     */
     @FXML
     private TextField usernameField;
+
+    /**
+     * The field where the user can enter the number of players to create a new game.
+     */
     @FXML
     private TextField numPlayersField;
 
+    /**
+     * The server interface.
+     */
     private ServerInterface serverInterface;
 
+    /**
+     * Initializes the controller. The username is kept when returning to the welcome screen from the games screen.
+     * @param url The URL.
+     * @param resourceBundle The resource bundle.
+     */
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         CodexGUI.getGUI().getGuiState().setWelcomeScreenController(this);
@@ -48,11 +66,19 @@ public class WelcomeScreenController implements ScreenController, Initializable 
         this.serverInterface = CodexGUI.getGUI().getServerInterface();
     }
 
+    /**
+     * Quits the application.
+     * @param ignored The action event (which is ignored).
+     */
     @FXML
     public void quit(ActionEvent ignored) {
         CodexGUI.getGUI().getStage().close();
     }
 
+    /**
+     * Joins a game.
+     * @param ignored The action event (which is ignored).
+     */
     @FXML
     public void join(ActionEvent ignored) {
         String username = usernameField.getText();
@@ -67,6 +93,10 @@ public class WelcomeScreenController implements ScreenController, Initializable 
         CodexGUI.getGUI().switchToGamesScreen();
     }
 
+    /**
+     * Creates a new game.
+     * @param ignored The action event (which is ignored).
+     */
     @FXML
     public void create(ActionEvent ignored) {
         String username = usernameField.getText();
@@ -99,6 +129,10 @@ public class WelcomeScreenController implements ScreenController, Initializable 
         }
     }
 
+    /**
+     * Makes it so that the given text field can only accept the numbers 2, 3 and 4.
+     * @param tf The text field to which this constraint should be applied.
+     */
     private static void makeNumOnly(final TextField tf) {
         tf.textProperty().addListener((ov, oldValue, newValue) -> {
                 if (!newValue.matches("[234]")) {
@@ -108,6 +142,11 @@ public class WelcomeScreenController implements ScreenController, Initializable 
         );
     }
 
+    /**
+     * Adds a text limiter to a text field.
+     * @param tf The text field to which this constraint should be applied.
+     * @param maxLength The maximum length.
+     */
     private static void addTextLimiter(final TextField tf, final int maxLength) {
         tf.textProperty().addListener((ov, oldValue, newValue) -> {
             if (tf.getText().length() > maxLength) {
@@ -117,6 +156,10 @@ public class WelcomeScreenController implements ScreenController, Initializable 
         });
     }
 
+    /**
+     * DOCME
+     * @param errorMessage
+     */
     @Override
     public void showError(String errorMessage) {
         //TODO implement
