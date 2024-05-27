c2b457f1f39a236f630d45898405311e32f4bc3f
lorenzodemontis02 <161143387+lorenzodemontis02@users.noreply.github.com>
2024-05-27 19:49:39 +0200

    Improved credits-screen.fxml's design

--- src/main/java/it/polimi/ingsw/am16/client/view/gui/controllers/screens/CreditsScreenController.java
+++ src/main/java/it/polimi/ingsw/am16/client/view/gui/controllers/screens/CreditsScreenController.java
@@ -1,10 +1,16 @@
 package it.polimi.ingsw.am16.client.view.gui.controllers.screens;
 
+import it.polimi.ingsw.am16.client.view.gui.CodexGUI;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.layout.VBox;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
+import javafx.scene.text.Text;
+
 
 import java.net.URISyntaxException;
 import java.net.URL;
@@ -14,9 +20,23 @@ public class CreditsScreenController implements Initializable {
 
     @FXML
     private MediaView rick;
+    @FXML
+    private VBox links;
+    @FXML
+    private VBox rickText;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    @FXML
+    public void click(ActionEvent ignored) {
+        System.out.println("Clicked");
+    }
+
+    @FXML
+    public void rick(ActionEvent ignored) {
         if(rick.getMediaPlayer() == null) {
             try {
                 String filename = getClass().getResource("/assets/gui/rk.mp4").toURI().toString();
@@ -27,7 +47,22 @@ public class CreditsScreenController implements Initializable {
                 e.printStackTrace();
             }
         }
+
+        rick.setVisible(true);
+        rick.requestFocus();
         rick.getMediaPlayer().seek(rick.getMediaPlayer().getStartTime());
         rick.getMediaPlayer().play();
+    }
+
+    @FXML
+    public void back(ActionEvent ignored) {
+        if(rick.getMediaPlayer().getStatus() == MediaPlayer.Status.PLAYING) {
+            rick.getMediaPlayer().stop();
+            rick.setVisible(false);
+            links.setVisible(false);
+            rickText.setVisible(true);
+        } else {
+            CodexGUI.getGUI().switchToWelcomeScreen();
+        }
     }
 }
