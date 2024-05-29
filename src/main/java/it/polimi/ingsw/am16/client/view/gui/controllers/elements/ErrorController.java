8edf286708b38e1e139d107c177911c57b8dfbb8
AndreaColombo02 <andrea78.colombo@mail.polimi.it>
2024-05-29 21:20:25 +0200

    Created fields in error.fxml for dynamic error handling

--- src/main/java/it/polimi/ingsw/am16/client/view/gui/controllers/elements/ErrorController.java
+++ src/main/java/it/polimi/ingsw/am16/client/view/gui/controllers/elements/ErrorController.java
@@ -4,14 +4,26 @@ import it.polimi.ingsw.am16.client.view.gui.CodexGUI;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ErrorController {
 
+    public Text errorText;
+    public Text butttonText;
+    public ImageView buttonIcon;
+
     @FXML
     public void quit(ActionEvent ignored) {
         CodexGUI.getGUI().getStage().close();
     }
+
+    public void setErrorText(String errorText) {
+        this.errorText.setText(errorText);
+    }
+
+
 }
