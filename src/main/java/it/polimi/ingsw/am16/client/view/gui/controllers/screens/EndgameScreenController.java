9a5f4f52784ae57d457503bb885465d6ad3015da
Leonardo Conti <leoconti2002@gmail.com>
2024-06-22 02:18:24 +0200

    Added a button to see players' play areas and objective cards in the endgame-screen.fxml

--- src/main/java/it/polimi/ingsw/am16/client/view/gui/controllers/screens/EndgameScreenController.java
+++ src/main/java/it/polimi/ingsw/am16/client/view/gui/controllers/screens/EndgameScreenController.java
@@ -2,6 +2,7 @@ package it.polimi.ingsw.am16.client.view.gui.controllers.screens;
 
 import it.polimi.ingsw.am16.client.view.gui.CodexGUI;
 import it.polimi.ingsw.am16.client.view.gui.controllers.elements.ErrorController;
+import it.polimi.ingsw.am16.client.view.gui.controllers.elements.PlayerButtonController;
 import it.polimi.ingsw.am16.client.view.gui.events.ErrorEvent;
 import it.polimi.ingsw.am16.client.view.gui.events.GUIEventTypes;
 import it.polimi.ingsw.am16.client.view.gui.util.ElementFactory;
@@ -9,8 +10,13 @@ import it.polimi.ingsw.am16.client.view.gui.util.ErrorFactory;
 import it.polimi.ingsw.am16.client.view.gui.util.GUIState;
 import it.polimi.ingsw.am16.client.view.gui.util.guiErrors.GUIError;
 import it.polimi.ingsw.am16.common.model.cards.ObjectiveCard;
+import it.polimi.ingsw.am16.common.util.FilePaths;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ContentDisplay;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
@@ -19,6 +25,7 @@ import javafx.scene.text.TextBoundsType;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 /**
  * Controller for the end game screen. The end game screen displays the amount of points for each player as well as the name of the winner(s) of the game.
@@ -39,6 +46,8 @@ public class EndgameScreenController {
     public VBox objectivePointsCol;
     @FXML
     public VBox totalPointsCol;
+    @FXML
+    public VBox infoCol;
 
     private GUIState guiState;
 
@@ -100,37 +109,56 @@ public class EndgameScreenController {
             Text gamePointsText = new Text(gamePoints.get(username).toString());
             Text objectivePointsText = new Text(objectivePoints.get(username).toString());
             Text totalPointsText = new Text(totalPoints.get(username).toString());
+            Button infoButton = new Button(username);
+            ImageView infoImage = new ImageView(new Image(Objects.requireNonNull(getClass().getResourceAsStream(FilePaths.GUI_ICONS + "/info.png"))));
 
             playerText.setBoundsType(TextBoundsType.LOGICAL_VERTICAL_CENTER);
             gamePointsText.setBoundsType(TextBoundsType.LOGICAL_VERTICAL_CENTER);
             objectivePointsText.setBoundsType(TextBoundsType.LOGICAL_VERTICAL_CENTER);
             totalPointsText.setBoundsType(TextBoundsType.LOGICAL_VERTICAL_CENTER);
 
+            infoButton.setPrefWidth(45);
+            infoButton.setPrefHeight(45);
+            infoButton.setContentDisplay(ContentDisplay.GRAPHIC_ONLY);
+            infoButton.setOnMousePressed(e -> {
+                //TODO
+            });
+
+            infoImage.setFitWidth(24);
+            infoImage.setFitHeight(24);
+            infoImage.setMouseTransparent(true);
+
             playerText.getStyleClass().add("endgame-text");
             gamePointsText.getStyleClass().add("endgame-text");
             objectivePointsText.getStyleClass().add("endgame-text");
             totalPointsText.getStyleClass().add("endgame-text");
             totalPointsText.getStyleClass().add("total");
+            infoButton.getStyleClass().add("transparent");
 
             StackPane playerPane = new StackPane();
             StackPane gamePointsPane = new StackPane();
             StackPane objectivePointsPane = new StackPane();
             StackPane totalPointsPane = new StackPane();
+            StackPane infoPane = new StackPane();
 
             playerPane.getChildren().add(playerText);
             gamePointsPane.getChildren().add(gamePointsText);
             objectivePointsPane.getChildren().add(objectivePointsText);
             totalPointsPane.getChildren().add(totalPointsText);
+            infoPane.getChildren().add(infoButton);
+            infoPane.getChildren().add(infoImage);
 
             playerPane.getStyleClass().add("endgame-cell");
             gamePointsPane.getStyleClass().add("endgame-cell");
             objectivePointsPane.getStyleClass().add("endgame-cell");
             totalPointsPane.getStyleClass().add("endgame-cell");
+            infoPane.getStyleClass().add("endgame-cell");
 
             playerCol.getChildren().add(playerPane);
             gamePointsCol.getChildren().add(gamePointsPane);
             objectivePointsCol.getChildren().add(objectivePointsPane);
             totalPointsCol.getChildren().add(totalPointsPane);
+            infoCol.getChildren().add(infoPane);
         }
     }
 
